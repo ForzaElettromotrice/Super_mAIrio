a256220c2defe2609d08e174c25d3ab7009e2b89
Dalphan0 <danyventurini25@gmail.com>
2024-05-23 10:22:22 +0200

    added new main with new mario

---
+++ src/main2.py
@@ -0,0 +1,88 @@
+import torch
+
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import RIGHT_ONLY
+import datetime
+from pathlib import Path
+
+# from agent import Agent
+
+from nes_py.wrappers import JoypadSpace
+from wrappers import apply_wrappers
+
+from mario2 import Mario
+
+import os
+
+# from utils import *
+
+ENV = 'SuperMarioBros-1-1-v0'
+TRAIN = True
+DISPLAY = True
+CKPT_SAVE_INTERVAL = 5000
+EPISODES = 10000
+
+def main():
+    save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    save_dir.mkdir(parents = True)
+
+    if torch.cuda.is_available():
+        print("Using CUDA device:", torch.cuda.get_device_name(0))
+    else:
+        print("CUDA is not available")
+
+    
+    env = gym_super_mario_bros.make(ENV, render_mode='human' if DISPLAY else 'rgb', apply_api_compatibility=True)
+    env = JoypadSpace(env, RIGHT_ONLY)
+    
+    env = apply_wrappers(env)
+    
+    mario = Mario(input_dims=env.observation_space.shape, num_actions=env.action_space.n)
+
+    if not TRAIN:
+        folder_name = ""
+        ckpt_name = ""
+        mario.load_model(os.path.join("models", folder_name, ckpt_name))
+        mario.epsilon = 0.2
+        mario.eps_min = 0.0
+        mario.eps_decay = 0.0
+    
+    env.reset()
+    next_state, reward, done, trunc, info = env.step(action=0)
+    
+    for e in range(EPISODES):
+        total_reward = 0
+        max_x = 0
+        done = False
+        
+        state, _ = env.reset()
+        
+        while not done:
+            if DISPLAY:
+                env.render()
+            
+            action = mario.do_action(next_state)
+            next_state, reward, done, trunc, info = env.step(action)
+            total_reward += reward
+            
+            max_x = max(info["x_pos"], max_x)
+            
+            if TRAIN:
+                mario.store_memory(state, action, reward, next_state, done)
+                mario.train()
+                
+            state = next_state
+            
+            if done:
+                break
+        
+        if TRAIN and (e + 1) % CKPT_SAVE_INTERVAL == 0:
+            mario.save_model(os.path.join(save_dir, "model_" + str(e + 1) + "_iter.pt"))
+        
+        
+        print(f"Episode {e + 1} Total reward: {total_reward}, Max x: {max_x}")
+        
+    env.close()
+
+if __name__ == "__main__":
+    main()
---
+++ src/mario2.py
@@ -0,0 +1,110 @@
+import torch
+import numpy as np
+from marionn import MarioNN
+
+from tensordict import TensorDict
+from torchrl.data import TensorDictReplayBuffer, LazyMemmapStorage
+
+class Mario:
+    def __init__(self, 
+                 input_dims, 
+                 num_actions, 
+                 learning_rate=0.00025, 
+                 gamma=0.9, 
+                 exploration_rate=1.0, 
+                 exploration_decay=0.99999975, 
+                 exploration_min=0.1, 
+                 replay_buffer_capacity=10_000, 
+                 batch_size=32, 
+                 sync_network_rate=10000):
+        
+        self.num_actions = num_actions
+        self.learn_step_counter = 0
+
+        # Hyperparameters
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.exploration_rate = exploration_rate
+        self.exploration_decay = exploration_decay
+        self.exploration_min = exploration_min
+        self.batch_size = batch_size
+        self.sync_network_rate = sync_network_rate
+
+        # Networks
+        self.online_network = MarioNN(input_dims, num_actions)
+        self.target_network = MarioNN(input_dims, num_actions, freeze=True)
+
+        # Optimizer and loss
+        self.optimizer = torch.optim.Adam(self.online_network.parameters(), lr=self.learning_rate)
+        self.loss = torch.nn.MSELoss()
+        # self.loss = torch.nn.SmoothL1Loss() # Feel free to try this loss function instead!
+
+        # Replay buffer
+        storage = LazyMemmapStorage(replay_buffer_capacity)
+        self.replay_buffer = TensorDictReplayBuffer(storage=storage)
+
+    def do_action(self, observation):
+        if np.random.random() < self.exploration_rate:
+            return np.random.randint(self.num_actions)
+
+        observation = torch.tensor(np.array(observation), dtype=torch.float32) \
+                        .unsqueeze(0) \
+                        .to(self.online_network.device)
+        # Grabbing the index of the action that's associated with the highest Q-value
+        return self.online_network(observation).argmax().item()
+    
+    def decay_exploration_rate(self):
+        self.exploration_rate = max(self.exploration_rate * self.exploration_decay, self.exploration_min)
+
+    def store_memory(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(TensorDict({
+                                            "state": torch.tensor(np.array(state), dtype=torch.float32), 
+                                            "action": torch.tensor(action),
+                                            "reward": torch.tensor(reward), 
+                                            "next_state": torch.tensor(np.array(next_state), dtype=torch.float32), 
+                                            "done": torch.tensor(done)
+                                          }, batch_size=[]))
+        
+    def sync_networks(self):
+        if self.learn_step_counter % self.sync_network_rate == 0 and self.learn_step_counter > 0:
+            self.target_network.load_state_dict(self.online_network.state_dict())
+
+    def save_model(self, path):
+        torch.save(self.online_network.state_dict(), path)
+
+    def load_model(self, path):
+        self.online_network.load_state_dict(torch.load(path))
+        self.target_network.load_state_dict(torch.load(path))
+
+    def train(self):
+        if len(self.replay_buffer) < self.batch_size:
+            return
+        
+        self.sync_networks()
+        
+        self.optimizer.zero_grad()
+
+        samples = self.replay_buffer.sample(self.batch_size).to(self.online_network.device)
+
+        keys = ("state", "action", "reward", "next_state", "done")
+
+        states, actions, rewards, next_states, dones = [samples[key] for key in keys]
+
+        predicted_q_values = self.online_network(states) # Shape is (batch_size, n_actions)
+        predicted_q_values = predicted_q_values[np.arange(self.batch_size), actions.squeeze()]
+
+        # Max returns two tensors, the first one is the maximum value, the second one is the index of the maximum value
+        target_q_values = self.target_network(next_states).max(dim=1)[0]
+        # The rewards of any future states don't matter if the current state is a terminal state
+        # If done is true, then 1 - done is 0, so the part after the plus sign (representing the future rewards) is 0
+        target_q_values = rewards + self.gamma * target_q_values * (1 - dones.float())
+
+        loss = self.loss(predicted_q_values, target_q_values)
+        loss.backward()
+        self.optimizer.step()
+
+        self.learn_step_counter += 1
+        self.decay_exploration_rate()
+
+
+        
